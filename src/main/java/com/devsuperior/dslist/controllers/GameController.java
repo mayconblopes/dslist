8403392818016de1853542185b4e3efa4d4a7210
Lopes, Maycon <lopes.maycon@gmail.com>
2025-01-23 17:59:00 -0300

    Get lists

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,7 +1,6 @@
 package com.devsuperior.dslist.controllers;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +17,7 @@ public class GameController {
 	
 	@Autowired
 	private GameService gameService;
+	
 	
 	@GetMapping
 	public List<GameMinDTO> findAll() {
